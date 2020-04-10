ea14ea1e687dca16d60edda4e098801de4d24495
lucaspellis <lucaspellis@hotmail.com>
2020-04-10 15:36:28 -0300

    First commit

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "ENTER YOURS HERE",
+    authDomain: "ENTER YOURS HERE",
+    databaseURL: "ENTER YOURS HERE",
+    projectId: "ENTER YOURS HERE",
+    storageBucket: "ENTER YOURS HERE",
+    messagingSenderId: "ENTER YOURS HERE"
+  }
